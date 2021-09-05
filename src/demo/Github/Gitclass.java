c46906aa7b0c389b3bdcc38a5b5696f6b027b599
DELL <DELL@Rushikesh>
2021-09-05 23:04:48 +0530

    plz look at this, this is commited project

---
+++ src/demo/Github/Gitclass.java
@@ -0,0 +1,12 @@
+package demo.Github;
+
+public class Gitclass {
+	
+	public static void main(String[] args) {
+		System.out.println("this is a program to know about the Git Repository");
+		System.out.println("please PULL it to update");
+		
+	
+	}
+
+}
